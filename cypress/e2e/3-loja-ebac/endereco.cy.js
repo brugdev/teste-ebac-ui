af2485fc2f5eb085ae0792a0d22afb81cdf72874
Bruno Gomes <bgomesma@gmail.com>
2023-12-26 21:44:19 -0300

    Usando arquivos de dados/testes de cadastros

--- cypress/e2e/3-loja-ebac/endereco.cy.js
+++ cypress/e2e/3-loja-ebac/endereco.cy.js
@@ -1,5 +1,8 @@
 ///<reference types="cypress"/>
+import enderecoPage from "../../support/page-objects/endereco.page";
+const dadosEndereco = require('../../fixtures/endereco.json')
 
+describe('Funcionalidade Enderecos - Faturamento e Entrega', () => {
 beforeEach(() => {
     cy.visit('minha-conta')
     cy.fixture('perfil').then(dados =>{
@@ -8,11 +11,17 @@ beforeEach(() => {
     })
 });
 
-describe('Funcionalidade Enderecos - Faturamento e Entrega', () => {
+
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-        
+       enderecoPage.editarEnderecoFaturamento('Maria','Marina', 'EBAC', 'Brasil', 'Rua Candido mendes', '45', 'Sao Benedito', 'Ceará', '29232-292', '21 49494933', 'marinateste@gmail.com') 
 
         
     });
+
+    it.only('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        enderecoPage.editarEnderecoFaturamento(dadosEndereco[1].nome,dadosEndereco[1].sobrenome,dadosEndereco[1].empresa,dadosEndereco[1].pais,dadosEndereco[1].endereco,dadosEndereco[1].numero,dadosEndereco[1].cidade,dadosEndereco[1].estado,dadosEndereco[1].cep,dadosEndereco[1].telefone,dadosEndereco[1].email) 
+ 
+         
+     });
     
 });
